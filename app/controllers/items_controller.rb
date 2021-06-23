05283a6585c4bce536b590cbf5a60627669a9131
noriaki-toma <do7nno@gmail.com>
2021-06-23 14:18:32 +0900

    新規登録機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,4 @@ class ItemsController < ApplicationController
     # @item = Item.all
   end
   
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
-  end
 end
