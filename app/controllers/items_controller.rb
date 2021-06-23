fc0f99f1ff4ffe834eb60432d5d63fb76b9b3296
noriaki-toma <do7nno@gmail.com>
2021-06-23 16:04:55 +0900

    ログイン、ログアウト機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :basic_auth
+  before_action :authenticate_user!, only: [:new ]
 
   def index 
-    # @item = Item.all
+    #@item = Item.all
   end
   
+  def new
+    @item = Item.new
+  end
+
 end
