a967245b1e6336dbc07ce8c33183d65418919b27
noriaki-toma <do7nno@gmail.com>
2021-06-24 16:03:46 +0900

    ユーザー管理機能の実装 修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください。' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください。' }
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数混合で入力してください。' }
-    validates :password_confirmation
     validates :birthday
   end
-  
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください。' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください。' }
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数混合で入力してください。' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,8 +52,8 @@ describe User do
         expect(@user.errors.full_messages).to include('Password は半角英数混合で入力してください。')
       end
       it 'passwordは全角英数混合では登録できない' do
-        @user.password = '111111'
-        @user.password_confirmation = '111111'
+        @user.password = 'AAAAAA'
+        @user.password_confirmation = 'AAAAAA'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password は半角英数混合で入力してください。')
       end
