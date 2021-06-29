4f55583aae5ecc7d1c0dde5d9f5a0000042c05e2
noriaki-toma <do7nno@gmail.com>
2021-06-29 16:41:23 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    category_id              { 3 }
+    condition_id             { 3 }
+    price                 { 1111 }
+    name                  { 'あ' }
+    text                  { 'あ' }
+    shipping_day_id { 3 }
+    postage_id { 3 }
+    prefecture_id { 3 }
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
