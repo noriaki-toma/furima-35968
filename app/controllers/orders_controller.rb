c1e32063fd758b0312c1d50c836d0627974f8832
noriaki-toma <do7nno@gmail.com>
2021-07-06 18:35:52 +0900

    購入のためのフォーム実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,30 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
   def index
     @order_address = OrderAddress.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal, :prefecture_id, :city, :address1, :address2, :telephone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
 
 end
